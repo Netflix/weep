2334cb861b690b9e9d11890633ceaf7964de9d6a
Patrick Sanders <psanders@netflix.com>
2020-10-19 19:45:18 -0700

    refactor bootstrap code, handle errors

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,8 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"github.com/manifoldco/promptui"
 	"github.com/mattn/go-isatty"
-	"github.com/netflix/weep/util"
-	"net/url"
 	"os"
 	"path"
 	"runtime"
@@ -39,13 +35,12 @@ func init() {
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
 func initConfig() {
-
 	if cfgFile != "" {
 		viper.SetConfigFile(cfgFile)
 	} else {
@@ -69,164 +64,19 @@ func initConfig() {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok && config.EmbeddedConfigFile != "" {
 			log.Debug("no config file found, trying to use embedded config")
 		} else if isatty.IsTerminal(os.Stdout.Fd()) {
-			firstRun()
+			err = config.FirstRunPrompt()
+			if err != nil {
+				log.Fatalf("config bootstrap failed: %v", err)
+			}
 		} else {
-			log.Fatalf("unable to read config file: %v", err)
+			log.Debug("unable to read config file: %v", err)
 		}
 	}
 
-	log.Debug("Found config")
+	log.Debug("found config at %s", viper.ConfigFileUsed())
 	if err := viper.Unmarshal(&config.Config); err != nil {
 		log.Fatalf("unable to decode config into struct: %v", err)
 	}
-}
-
-func firstRun() {
-	fmt.Println("Welcome to weep! It looks like this is your first time running.")
-	fmt.Println("We're going to ask a few questions to get you set up.")
-	fmt.Println("Or, if you would prefer, you can manually create a config file.")
-	fmt.Println("Learn more here: https://github.com/Netflix/weep#configuration")
-	cmURL, err := promptConsoleMeURL()
-	if err != nil {
-		// TODO: handle error
-	}
-	viper.Set("consoleme_url", cmURL)
-
-	authMethod, err := promptAuthMethod()
-	if err != nil {
-		// TODO: handle error
-	}
-	viper.Set("authentication_method", authMethod)
-
-	if authMethod == "mtls" {
-		cert, err := promptFilePath("mTLS certificate path", "")
-		if err != nil {
-			// TODO: handle error
-		}
-		viper.Set("mtls_settings.cert", cert)
-
-		key, err := promptFilePath("mTLS key path", "")
-		if err != nil {
-			// TODO: handle error
-		}
-		viper.Set("mtls_settings.key", key)
-
-		ca, err := promptFilePath("mTLS CA bundle path", "")
-		if err != nil {
-			// TODO: handle error
-		}
-		viper.Set("mtls_settings.cafile", ca)
-
-		insecure, err := promptBool("Skip validation of mTLS hostname?")
-		if err != nil {
-			// TODO: handle error
-		}
-		viper.Set("mtls_settings.insecure", insecure)
-	} else if authMethod == "challenge" {
-
-	}
-
-	home, err := homedir.Dir()
-	if err != nil {
-		// TODO: handle error
-	}
-	defaultConfig := path.Join(home, ".weep.yaml")
-	saveLocation, err := promptFilePathNoValidate("Config destination", defaultConfig)
-	err = viper.SafeWriteConfigAs(saveLocation)
-	if err != nil {
-		// TODO: handle error
-	}
-}
-
-func promptConsoleMeURL() (string, error) {
-	validateURL := func(input string) error {
-		_, err := url.ParseRequestURI(input)
-		if err != nil {
-			return errors.New("Invalid URL")
-		}
-		return nil
-	}
-
-	prompt := promptui.Prompt{
-		Label:    "ConsoleMe URL",
-		Validate: validateURL,
-	}
-
-	result, err := prompt.Run()
-
-	if err != nil {
-		return "", err
-	}
-
-	return result, nil
-}
-
-func promptAuthMethod() (string, error) {
-	prompt := promptui.Select{
-		Label: "Authentication method",
-		Items: []string{"challenge", "mtls"},
-	}
-
-	_, result, err := prompt.Run()
-
-	if err != nil {
-		return "", err
-	}
-
-	return result, nil
-}
-
-func promptFilePath(label, default_value string) (string, error) {
-	validateFile := func(input string) error {
-		if util.FileExists(input) {
-			return nil
-		} else {
-			return fmt.Errorf("File not found: %s", input)
-		}
-	}
-	prompt := promptui.Prompt{
-		Label:    label,
-		Validate: validateFile,
-		Default:  default_value,
-	}
-
-	result, err := prompt.Run()
-
-	if err != nil {
-		return "", err
-	}
-
-	return result, nil
-}
-
-func promptFilePathNoValidate(label, default_value string) (string, error) {
-	prompt := promptui.Prompt{
-		Label:   label,
-		Default: default_value,
-	}
-
-	result, err := prompt.Run()
-
-	if err != nil {
-		return "", err
-	}
-
-	return result, nil
-}
-
-func promptBool(label string) (bool, error) {
-	prompt := promptui.Select{
-		Label: label,
-		Items: []string{"true", "false"},
-	}
-
-	index, _, err := prompt.Run()
-
-	if err != nil {
-		return false, err
-	}
-
-	return index == 0, nil
 }
 
 func initLogging() {
---
+++ config/bootstrap.go
@@ -0,0 +1,162 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"github.com/manifoldco/promptui"
+	"github.com/mitchellh/go-homedir"
+	"github.com/netflix/weep/util"
+	"github.com/spf13/viper"
+	"net/url"
+	"path"
+)
+
+// FirstRunPrompt gets user input to bootstrap a bare-minimum configuration.
+func FirstRunPrompt() error {
+	fmt.Println("Welcome to weep, the ConsoleMe CLI!")
+	fmt.Println("We're going to ask a few questions to get you set up.")
+	fmt.Println("Or, if you would prefer, you can manually create a config file.")
+	fmt.Println("Learn more here: https://github.com/Netflix/weep#configuration")
+	cmURL, err := promptConsoleMeURL()
+	if err != nil {
+		return err
+	}
+	viper.Set("consoleme_url", cmURL)
+
+	authMethod, err := promptAuthMethod()
+	if err != nil {
+		return err
+	}
+	viper.Set("authentication_method", authMethod)
+
+	if authMethod == "mtls" {
+		cert, err := promptFilePath("mTLS certificate path", "")
+		if err != nil {
+			return err
+		}
+		viper.Set("mtls_settings.cert", cert)
+
+		key, err := promptFilePath("mTLS key path", "")
+		if err != nil {
+			return err
+		}
+		viper.Set("mtls_settings.key", key)
+
+		ca, err := promptFilePath("mTLS CA bundle path", "")
+		if err != nil {
+			return err
+		}
+		viper.Set("mtls_settings.cafile", ca)
+
+		insecure, err := promptBool("Skip validation of mTLS hostname?")
+		if err != nil {
+			return err
+		}
+		viper.Set("mtls_settings.insecure", insecure)
+	} else if authMethod == "challenge" {
+
+	}
+
+	home, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+	defaultConfig := path.Join(home, ".weep.yaml")
+	saveLocation, err := promptFilePathNoValidate("Config destination", defaultConfig)
+	err = viper.SafeWriteConfigAs(saveLocation)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func promptConsoleMeURL() (string, error) {
+	validateURL := func(input string) error {
+		_, err := url.ParseRequestURI(input)
+		if err != nil {
+			return errors.New("invalid URL")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "ConsoleMe URL",
+		Validate: validateURL,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
+func promptAuthMethod() (string, error) {
+	prompt := promptui.Select{
+		Label: "Authentication method",
+		Items: []string{"challenge", "mtls"},
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
+func promptFilePath(label, defaultValue string) (string, error) {
+	validateFile := func(input string) error {
+		if util.FileExists(input) {
+			return nil
+		} else {
+			return fmt.Errorf("file not found: %s", input)
+		}
+	}
+	prompt := promptui.Prompt{
+		Label:    label,
+		Validate: validateFile,
+		Default:  defaultValue,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
+func promptFilePathNoValidate(label, defaultValue string) (string, error) {
+	prompt := promptui.Prompt{
+		Label:   label,
+		Default: defaultValue,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
+func promptBool(label string) (bool, error) {
+	prompt := promptui.Select{
+		Label: label,
+		Items: []string{"true", "false"},
+	}
+
+	index, _, err := prompt.Run()
+
+	if err != nil {
+		return false, err
+	}
+
+	return index == 0, nil
+}
