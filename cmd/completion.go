bf6f99f7229aba3b4686807421bf7105c9c9e78e
Curtis Castrapel <ccastrapel@netflix.com>
2020-10-29 12:20:04 -0700

    Remove unecessary comments

--- cmd/completion.go
+++ cmd/completion.go
@@ -47,14 +47,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(completionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// completionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// completionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
