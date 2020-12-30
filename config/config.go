77cef7421624b5cefd6173a3c3f8a7d668d8888e
Patrick Sanders <psanders@netflix.com>
2020-12-30 15:48:43 -0800

    Save challenge username to config file (#35)

--- config/config.go
+++ config/config.go
@@ -17,8 +17,13 @@
 package config
 
 import (
+	"path"
 	"path/filepath"
 	"runtime"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/spf13/viper"
 )
@@ -47,6 +52,98 @@ func getDefaultLogFile() string {
 	default:
 		return ""
 	}
+}
+
+// initConfig reads in configs by precedence, with later configs overriding earlier:
+//   - embedded
+//   - /etc/weep/weep.yaml
+//   - ~/.weep/weep.yaml
+//   - ./weep.yaml
+// If a config file is specified via CLI arg, it will be read exclusively and not merged with other
+// configuration.
+func InitConfig(filename string) error {
+	viper.SetConfigType("yaml")
+
+	// Read in explicitly defined config file
+	if filename != "" {
+		viper.SetConfigFile(filename)
+		if err := viper.ReadInConfig(); err != nil {
+			log.Errorf("could not open config file %s: %v", filename, err)
+			return err
+		}
+		return nil
+	}
+
+	// Read embedded config if available
+	if err := ReadEmbeddedConfig(); err != nil {
+		log.Debugf("unable to read embedded config: %v", err)
+	}
+
+	configLocations := []string{
+		"/etc/weep",
+		"$HOME/.weep",
+		".",
+	}
+
+	for _, dir := range configLocations {
+		viper.SetConfigName("weep")
+		viper.AddConfigPath(dir)
+		if err := viper.MergeInConfig(); err != nil {
+			return errors.Wrapf(err, "failed to read config from %s", viper.ConfigFileUsed())
+		}
+	}
+
+	// TODO: revisit first-run setup
+	//if err := viper.MergeInConfig(); err != nil {
+	//	if _, ok := err.(viper.ConfigFileNotFoundError); ok && config.EmbeddedConfigFile != "" {
+	//		log.Debugf("no config file found, trying to use embedded config")
+	//	} else if isatty.IsTerminal(os.Stdout.Fd()) {
+	//		err = util.FirstRunPrompt()
+	//		if err != nil {
+	//			log.Fatalf("config bootstrap failed: %v", err)
+	//		}
+	//	} else {
+	//		log.Debugf("unable to read config file: %v", err)
+	//	}
+	//}
+
+	if err := viper.Unmarshal(&Config); err != nil {
+		return errors.Wrap(err, "unable to decode config into struct")
+	}
+	return nil
+}
+
+// SetUser saves the provided username to ~/.weep/weep.yaml
+func SetUser(user string) error {
+	// Create a temporary viper instance to isolate from main config
+	v := viper.New()
+	v.SetConfigType("yaml")
+	v.SetConfigName("weep")
+	v.AddConfigPath("$HOME/.weep")
+
+	// Read in existing config if there is one so we don't overwrite it
+	if err := v.ReadInConfig(); err != nil {
+		switch err.(type) {
+		case viper.ConfigFileNotFoundError:
+			break
+		default:
+			return err
+		}
+	}
+
+	// Set user in the temp config then write to file
+	v.Set("challenge_settings.user", user)
+
+	home, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+	configPath := path.Join(home, ".weep/weep.yaml")
+
+	if err := v.WriteConfigAs(configPath); err != nil {
+		return err
+	}
+	return nil
 }
 
 var (
