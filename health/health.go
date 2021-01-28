3d1068ba3d1849f77c3602395e68b302804222ec
Patrick Sanders <psanders@netflix.com>
2021-01-27 16:33:31 -0800

    clean up status mutex

--- health/health.go
+++ health/health.go
@@ -5,35 +5,34 @@ import "sync"
 var WeepStatus status
 
 type status struct {
+	sync.RWMutex
 	healthy bool
 	reason  string
-	m       *sync.RWMutex
 }
 
 func init() {
 	WeepStatus = status{
 		healthy: true,
 		reason:  "healthy",
-		m:       &sync.RWMutex{},
 	}
 }
 
 func (s *status) Get() (bool, string) {
-	s.m.RLock()
-	defer s.m.RUnlock()
+	s.RLock()
+	defer s.RUnlock()
 	return s.healthy, s.reason
 }
 
 func (s *status) SetUnhealthy(reason string) {
-	s.m.Lock()
-	defer s.m.Unlock()
+	s.Lock()
+	defer s.Unlock()
 	s.healthy = false
 	s.reason = reason
 }
 
 func (s *status) SetHealthy() {
-	s.m.Lock()
-	defer s.m.Unlock()
+	s.Lock()
+	defer s.Unlock()
 	s.healthy = true
 	s.reason = "healthy"
 }
